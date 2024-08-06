b68d8edcc9cae79a977b2b457ada49d4128de5f9
kratikaFlipkart <kratika.dubey@flipkart.com>
2024-08-06 16:16:02 +0530

    first commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * 
+	 */
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
